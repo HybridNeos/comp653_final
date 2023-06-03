fb9dd3165d90513ab20d22c60f464802096005c1
Mark Shapiro <mark3198@gmail.com>
2023-06-02 20:57:40 -0400

    Initial data

---
+++ insurance_claims/models/insurance_claims.sql
@@ -0,0 +1,39 @@
+SELECT
+    months_as_customer,
+    -- age discriminatory
+    -- policy_number not useful
+    policy_bind_date,
+    policy_state,
+    policy_csl,
+    policy_deductable,
+    policy_annual_premium,
+    umbrella_limit,
+    -- insured_zip too granular
+    -- insured_sex discriminatory
+    insured_education_level,
+    insured_occupation,
+    -- insured_hobbies not useful (maybe)
+    insured_relationship,
+    capital_gains,
+    capital_loss,
+    incident_date,
+    collision_type,
+    incident_severity,
+    authorities_contacted,
+    incident_state,
+    -- incident_city too granular
+    -- incident_location too granular
+    incident_hour_of_the_day,
+    number_of_vehicles_involved,
+    property_damage,
+    bodily_injuries,
+    witnesses,
+    police_report_available,
+    total_claim_amount,
+    injury_claim,
+    property_claim,
+    vehicle_claim,
+    --auto_make decide if want to join to https://www.kaggle.com/datasets/sidharth178/car-prices-dataset
+    --auto_model,
+FROM
+    {{ ref('raw_insurance_claims') }}
